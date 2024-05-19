6e0152125f452a7a8dd4b536805b13a5e1127af4
Kalen <116669445+kaleper@users.noreply.github.com>
2024-05-19 01:30:24 +0000

    Initial commit

---
+++ config.php
@@ -0,0 +1,9 @@
+<?php
+
+define ('DB_HOST',          eld.h.filess.io);
+define ('DB_USER',          'cosc4806_presentwet');
+define ('DB_PASS',          $_ENV['DB_PASS']);  
+define ('DB_DATABASE',      'cosc4806_presentwet');
+define ('DB_PORT',          '3305');
+
+?>
